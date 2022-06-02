b335f1156c7927d3e8f037e54685340c5a493483
Christian <cetischristianocana@gmail.com>
2022-06-02 12:41:10 -0500

    Front End

---
+++ sidebar.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementsById("main-content").style.marginLeft = "250px";
+}
+
+
+function closeNav(){
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main-content").style.marginLeft = "0";
+}
